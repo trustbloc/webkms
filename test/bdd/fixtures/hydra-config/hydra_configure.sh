f5eb05cf0f9c35a81ef4225512ab857afaad0e14
Rolson Quadras <rolson.quadras@securekey.com>
2022-04-21 15:07:17 -0400

    test: rename hub-auth to auth
    Signed-off-by: Rolson Quadras <rolson.quadras@securekey.com>

--- test/bdd/fixtures/hydra-config/hydra_configure.sh
+++ test/bdd/fixtures/hydra-config/hydra_configure.sh
@@ -5,13 +5,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-echo "Creating client for hub-auth..."
+echo "Creating client for auth..."
 # will use --skip-tls-verify because hydra doesn't trust self-signed certificate
 # remove it when using real certificate
 hydra clients create \
     --endpoint https://oidc.provider.example.com:5556 \
-    --id hub-auth \
-    --secret hub-auth-secret \
+    --id auth \
+    --secret auth-secret \
     --grant-types authorization_code,refresh_token \
     --response-types code,id_token \
     --scope openid,profile,email \
